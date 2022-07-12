9d716e7f2f23ea96dfcea35a53ae877068ffdeee
vncscst <vncs.cst@gmail.com>
2022-07-12 19:40:08 -0300

    organização bibliotecas

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,5 @@
-#include <Arduino.h>
-
-  //Bibliotecas Wifi
-#if defined(ESP8266)
-#include <ESP8266WiFi.h>
-#elif defined(ESP32)
-#include <WiFi.h>
-#include <WebServer.h>
-#else
-#error Invalid platform
-#endif
-#include <DNSServer.h>
-#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
-
-//Bibliotecas Time
-#include <Time.h>
-#include <TimeLib.h>
-
-//Bibliotecas MQTT
-#include <PubSubClient.h>
-
-//Bibliotecas GERAIS
-#include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
-#include <Wire.h>
-#include "Adafruit_HTU21DF.h"
+//Bibliotecas
+#include "header.h"
 
 //PINOS
 #define gpioBtnWm 0
